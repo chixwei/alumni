c45962a22220780f6efa8d6b09c1395b78b77415
chixwei <78296446+chixwei@users.noreply.github.com>
2021-02-02 17:55:20 +0800

    cacat home page
    dh seperate section only one page

--- home.php
+++ home.php
@@ -3,6 +3,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css">
+        <title> Kaweiee University </title>
     </head>
     <body>
 
@@ -10,6 +11,29 @@
             <source src="homevideo.mp4" type="video/mp4">
                 Your browser does not support HTML5 video.
         </video>
+
+        <header>
+            <div class="wrapper">
+
+                <div class="logo">
+                    <img src="logo.png" alt="Kaweiee Logo">
+                </div>
+
+                <ul class="nav">
+                    <li><a href="home.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <li><a href="event.php">Event</a></li>
+                </ul>
+
+            </div>
+
+            <div class="welcome">
+                <h1>"NEVER GIVE UP BECAUSE GREAT THINGS TAKE TIME"</h1>
+                <!-- not sure login or sign up -->
+                <a href="login.php"> Explore</a>
+            </div>
+        </header>
 
         
     </body>
