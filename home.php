bd56cb79efdecfeda0613fe755a94d326eb3f698
chixwei <78296446+chixwei@users.noreply.github.com>
2021-02-02 16:09:09 +0800

    background video
    only video

--- home.php
+++ home.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
     <head>
-
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
+
+        <video autoplay muted loop id="homevideo">
+            <source src="homevideo.mp4" type="video/mp4">
+                Your browser does not support HTML5 video.
+        </video>
+
         
     </body>
 </html>
