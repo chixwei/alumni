8d3e1951587fccc5442efce84c5ce3e24671d0ab
chixwei <78296446+chixwei@users.noreply.github.com>
2021-02-05 20:06:07 +0800

    background and logo
    pure background , logo and overlay

--- home.php
+++ home.php
@@ -3,28 +3,23 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css">
-        <title> Kaweiee University </title>
+        <title> Kaweiee University</title>
     </head>
     <body>
-
+        
         <video autoplay muted loop id="homevideo">
             <source src="homevideo.mp4" type="video/mp4">
-                Your browser does not support HTML5 video.
+                Your browser does not support HTML5 video
         </video>
 
         <header>
-            
             <div class="logo">
                 <img src="logo.png" alt="Kaweiee Logo">
             </div>
 
-            <div class="welcome">
-                <h1>"NEVER GIVE UP BECAUSE GREAT THINGS TAKE TIME"</h1>
-                <!-- not sure login or sign up -->
-                <a href="signup.php"> Explore</a>
-            </div>
+            <div class="overlay"></div>
+
 
         </header>
-
     </body>
 </html>
