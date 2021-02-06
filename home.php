3e2b0a42b6311e71de0e8e3d427d74fa73f9bf30
chixwei <78296446+chixwei@users.noreply.github.com>
2021-02-06 12:17:08 +0800

    fail section
    section fail

--- home.php
+++ home.php
@@ -6,20 +6,47 @@
         <title> Kaweiee University</title>
     </head>
     <body>
-        
+        <div class="vid">
         <video autoplay muted loop id="homevideo">
             <source src="homevideo.mp4" type="video/mp4">
                 Your browser does not support HTML5 video
         </video>
+        </div>
 
         <header>
             <div class="logo">
-                <img src="logo.png" alt="Kaweiee Logo">
+                <img src="homelogo.png" alt="Kaweiee Logo">
             </div>
 
             <div class="overlay"></div>
 
+            <div class="content text-md-center">
+                <h1>Welcome to Kaweiee University</h1>
+                <p>"Never Give Up Because Great Things Take Time"</p>
+                <a href="login.php"> Explore More</a>
+            </div>
 
         </header>
+        <main>
+            <section class="section-a">
+
+                <div class="container">
+                    <h2>Section A</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non ad, ipsum aliquam laudantium amet eaque corporis vel culpa excepturi reiciendis!</p>
+                </div>
+    
+            </section>
+    
+            <section class="section-b">
+    
+                <div class="container">
+                    <h2>Section B</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non ad, ipsum aliquam laudantium amet eaque corporis vel culpa excepturi reiciendis!</p>
+                </div>
+                
+            </section>
+            
+        </main>
+        
     </body>
 </html>
