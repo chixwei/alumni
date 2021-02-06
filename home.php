68da85d243f2843dfd10688a5403302c14b69dde
chixwei <78296446+chixwei@users.noreply.github.com>
2021-02-07 00:53:37 +0800

    settle section

--- home.php
+++ home.php
@@ -6,47 +6,53 @@
         <title> Kaweiee University</title>
     </head>
     <body>
-        <div class="vid">
-        <video autoplay muted loop id="homevideo">
-            <source src="homevideo.mp4" type="video/mp4">
-                Your browser does not support HTML5 video
-        </video>
-        </div>
+        
 
         <header>
+
             <div class="logo">
                 <img src="homelogo.png" alt="Kaweiee Logo">
             </div>
 
+            <div class="vid">
+                <video autoplay muted loop id="homevideo">
+                    <source src="homevideo.mp4" type="video/mp4">
+                        Your browser does not support HTML5 video
+                </video>
+            </div>
+
             <div class="overlay"></div>
 
-            <div class="content text-md-center">
+            <div class="content">
                 <h1>Welcome to Kaweiee University</h1>
                 <p>"Never Give Up Because Great Things Take Time"</p>
-                <a href="login.php"> Explore More</a>
+                <a href="login.php"> Explore More</a> 
             </div>
 
         </header>
-        <main>
-            <section class="section-a">
 
-                <div class="container">
-                    <h2>Section A</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non ad, ipsum aliquam laudantium amet eaque corporis vel culpa excepturi reiciendis!</p>
-                </div>
-    
-            </section>
-    
-            <section class="section-b">
-    
-                <div class="container">
-                    <h2>Section B</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non ad, ipsum aliquam laudantium amet eaque corporis vel culpa excepturi reiciendis!</p>
-                </div>
-                
-            </section>
+
+        <section class="section-a">
+
+            <div class="container">
+              <h2>Section A</h2>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non ad, ipsum aliquam laudantium amet eaque corporis vel culpa excepturi reiciendis!</p>
+            </div>
             
-        </main>
+          </section>
+          
+          <section class="section-b">
+          
+            <div class="container">
+              <h2>Section B</h2>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non ad, ipsum aliquam laudantium amet eaque corporis vel culpa excepturi reiciendis!</p>
+            </div>
+            
+          </section>
+          
+          <footer>
+            <p>Footer</p>
+          </footer>
         
     </body>
 </html>
